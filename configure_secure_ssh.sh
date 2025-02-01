0e47497b995a1091816356ea0f49e7994053363f
rusakovcom <118691484+rusakovcom@users.noreply.github.com>
2025-02-01 13:27:46 +0300

    Update configure_secure_ssh.sh

--- configure_secure_ssh.sh
+++ configure_secure_ssh.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# configure secure ssh on new server
+
+# configure secure ssh for new vps
 # curl -O https://raw.githubusercontent.com/rusakovcom/tools/main/configure_secure_ssh.sh
 
-SSH_PORT='22121'
+# usage:
+# ./configure_secure_ssh.sh <port for ssh> '<public ssh key>'
+
+SSH_PORT=$1
+SSH_KEY=$2
 
 NEW_CONFIG=$(cat <<EOF  
 Port $SSH_PORT
@@ -18,4 +23,10 @@ EOF
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup.$(date +%Y%m%d_%H%M%S)
 echo "$NEW_CONFIG" > /etc/ssh/sshd_config
 
-systemctl restart sshd
+sshd -t
+if [ $? -eq 0 ]; then
+    systemctl restart sshd
+else
+    cp /etc/ssh/sshd_config.backup.* /etc/ssh/sshd_config
+    exit 1
+fi
