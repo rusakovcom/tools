57e4a49f6adecaed9b9a0393cfea99e4266d3fea
rusakovcom <118691484+rusakovcom@users.noreply.github.com>
2025-02-01 13:10:55 +0300

    Update configure_secure_ssh.sh

--- configure_secure_ssh.sh
+++ configure_secure_ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup.$(date +%Y%m%d_%H%M%S)
+# configure secure ssh on new server
+# curl -O https://raw.githubusercontent.com/rusakovcom/tools/main/configure_secure_ssh.sh
 
 SSH_PORT='22121'
 
@@ -9,12 +9,13 @@ Port $SSH_PORT
 PermitRootLogin prohibit-password
 PasswordAuthentication no
 PubkeyAuthentication yes
-PermitEmptyPasswords no
+X11Forwarding no
 AllowTcpForwarding no
 MaxAuthTries 3
 EOF
 )
 
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup.$(date +%Y%m%d_%H%M%S)
 echo "$NEW_CONFIG" > /etc/ssh/sshd_config
 
 systemctl restart sshd
