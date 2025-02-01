7b6181c9603ea28be240c6bf5c767fe24786b4b3
rusakovcom <118691484+rusakovcom@users.noreply.github.com>
2025-02-01 13:34:32 +0300

    Update configure_secure_ssh.sh

--- configure_secure_ssh.sh
+++ configure_secure_ssh.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# configure secure ssh for new vps
+# configure secure ssh for new vps (root access)
 # curl -O https://raw.githubusercontent.com/rusakovcom/tools/main/configure_secure_ssh.sh
 
-# usage:
+# usage example:
 # ./configure_secure_ssh.sh <port for ssh> '<public ssh key>'
+# ./configure_secure_ssh.sh 22123 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK0wmN/Cr3JXqmLW7u+g9pTh+wyqDHpSQEIQczXkVx9q anton@rusakov.org'
 
 SSH_PORT=$1
 SSH_KEY=$2
@@ -22,6 +23,7 @@ EOF
 
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup.$(date +%Y%m%d_%H%M%S)
 echo "$NEW_CONFIG" > /etc/ssh/sshd_config
+echo "$SSH_KEY" >> /root/.ssh/authorized_keys
 
 sshd -t
 if [ $? -eq 0 ]; then
